fb38fe0b17ce3e1a67427f6465fca20c8f72aa5d
Tobilinski <Tobiassherco125@gmail.com>
2025-03-09 22:03:37 +0200

    MainMenu looking smooth

--- ReturnOfTheShadow/Assets/_Assets/Scripts/Menu/Menu.cs
+++ ReturnOfTheShadow/Assets/_Assets/Scripts/Menu/Menu.cs
@@ -1,13 +1,31 @@
+using Lean.Gui;
 using UnityEngine;
 using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private Button playButton;
+    [SerializeField] private Button levelBackButton;
     [SerializeField] private Button quitButton;
+    [SerializeField] private LeanWindow landingPageLeanWindow;
+    [SerializeField] private LeanWindow levelSelectLeanWindow;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playButton.onClick.AddListener(CloseLandingPage);
+        levelBackButton.onClick.AddListener(CloseLevelSelect);
         quitButton.onClick.AddListener(Application.Quit);
+    }
+
+    private void CloseLandingPage()
+    {
+        landingPageLeanWindow.TurnOff();
+        levelSelectLeanWindow.TurnOn();
+    }
+
+    private void CloseLevelSelect()
+    {
+        levelSelectLeanWindow.TurnOff();
+        landingPageLeanWindow.TurnOn();
     }
 }
