7ffafbfbe7d4ddf2031a964b66fd8ad0b933c7ad
Tobilinski <Tobiassherco125@gmail.com>
2025-03-11 20:31:59 +0200

    Null refs fixed

--- ReturnOfTheShadow/Assets/_Assets/Scripts/Menu/Menu.cs
+++ ReturnOfTheShadow/Assets/_Assets/Scripts/Menu/Menu.cs
@@ -6,11 +6,8 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private Button playButton;
-    private LeanHover playHover;
     [SerializeField] private Button quitButton;
-    [SerializeField] private Button levelBackButton;
     [SerializeField] private LeanWindow landingPageLeanWindow;
-    [SerializeField] private LeanWindow levelSelectLeanWindow;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,22 +18,11 @@ public class Menu : MonoBehaviour
     private void SetUp()
     {
         playButton.onClick.AddListener(CloseLandingPage);
-        playHover = playButton.gameObject.AddComponent<LeanHover>();
-        levelBackButton.onClick.AddListener(CloseLevelSelect);
         quitButton.onClick.AddListener(Application.Quit);
     }
 
     private void CloseLandingPage()
     {
         landingPageLeanWindow.TurnOff();
-        levelSelectLeanWindow.TurnOn();
     }
-
-    private void CloseLevelSelect()
-    {
-        levelSelectLeanWindow.TurnOff();
-        landingPageLeanWindow.TurnOn();
-    }
-
-   
 }
