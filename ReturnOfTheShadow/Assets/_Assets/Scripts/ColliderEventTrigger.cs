8586e9cda609f880f9279dcdb25bb305a9c2f62e
Tobilinski <Tobiassherco125@gmail.com>
2025-04-01 23:03:21 +0200

    Added bunch new events in collider event, New player parent script, and enw anims

--- ReturnOfTheShadow/Assets/_Assets/Scripts/ColliderEventTrigger.cs
+++ ReturnOfTheShadow/Assets/_Assets/Scripts/ColliderEventTrigger.cs
@@ -1,24 +1,45 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Sirenix.OdinInspector;
 
-public class ColliderEventTrigger : MonoBehaviour
+public class ColliderEventTrigger : SerializedMonoBehaviour
 {
+    [Title("Other Collider Colliding List")] 
+    [SerializeField] private HashSet<Collider2D> colliders;
+    [Title("Unity Events")]
     public UnityEvent onCollisionEnter;
+    public UnityEvent<PlayerController> onCollisionEnterPlayer;
     public UnityEvent<GameObject> onCollisionEnterBoomerang;
+    public UnityEvent onCollisionExit;
+    public UnityEvent<PlayerController> onCollisionExitPlayer;
     public void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject != null)
-        {
-            onCollisionEnter.Invoke();
-        }
+        colliders.Add(other.gameObject.GetComponent<Collider2D>());
+        
+        onCollisionEnter?.Invoke();
         if (other.gameObject.TryGetComponent(out IInteractable interactable))
         {
             interactable.Interact();
         }
-        if (other.gameObject.CompareTag("Boomerang"))
+        else if (other.gameObject.CompareTag("Boomerang"))
         {
-            onCollisionEnterBoomerang.Invoke(other.gameObject);
+            onCollisionEnterBoomerang?.Invoke(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("Player"))
+        {
+            onCollisionEnterPlayer?.Invoke(other.gameObject.GetComponent<PlayerController>());
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D other)
+    {
+        colliders.Remove(other.gameObject.GetComponent<Collider2D>());
+        onCollisionExit?.Invoke();
+        if(other.gameObject.CompareTag("Player"))
+        {
+            onCollisionExitPlayer?.Invoke(other.gameObject.GetComponent<PlayerController>());
         }
     }
 }
